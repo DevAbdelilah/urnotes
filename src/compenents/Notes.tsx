c20440b481c7c9cf7392544977d089b943af3401
DevAbdelilah <charafiabdelilah3@gmail.com>
2024-06-19 19:45:53 +0200

    add searche note and input note

--- src/compenents/Notes.tsx
+++ src/compenents/Notes.tsx
@@ -1,6 +1,6 @@
 import { InputBase, Stack, useTheme } from "@mui/material";
 import { grey } from "@mui/material/colors";
-import { useEffect } from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Notes() {
   const theme = useTheme();
@@ -8,14 +8,35 @@ function Notes() {
   const isDarkMode = theme.palette.mode === "dark";
 
   return (
-    <Stack p={"16px 24px"}>
+    <Stack>
+      <Stack pl={"50px"} alignItems={"center"}>
+        <Stack
+          bgcolor={isDarkMode ? grey[600] : grey[100]}
+          width={"80%"}
+          p={"8px 24px"}
+          borderRadius={"40px"}
+          direction={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <InputBase sx={{ width: "100%" }} placeholder="Search for a note" />
+          <SearchIcon />
+        </Stack>
+      </Stack>
       <Stack
-        bgcolor={isDarkMode ? grey[600] : grey[100]}
-        maxWidth={"300px"}
-        p={"8px 24px"}
-        borderRadius={"40px"}
+        m={" 25px 50px"}
+        borderRadius={"8px"}
+        width={"350px"}
+        height={"200px"}
+        bgcolor={"#dddddd"}
       >
-        <InputBase placeholder="Search for a note" />
+        <Stack sx={{ width: "100%", height: "100%", padding: "8px" }}>
+          <InputBase
+            placeholder=" Add your Note"
+
+            // Ensure padding is included in the width
+          />
+        </Stack>
       </Stack>
     </Stack>
   );
